4e41d312ab766db82de0333a648d121f7050d619
Josh Warwick <josh.warwick15@gmail.com>
2022-12-07 22:59:55 +0000

    day3

---
+++ p3a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+from utils import *
+
+input = load_input_as_list()
+
+
+def split(x):
+    half = len(x) // 2
+    return (x[:half], x[half:])
+
+
+def doit(input):
+    xs = [set(x) & set(y) for x, y in map(split, input)]
+    return sum([ord(a) - 38 if a.isupper() else ord(a) - 96 for (a,) in xs])
+
+
+if __name__ == "__main__":
+    print(doit(input))
---
+++ p3b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+from utils import *
+
+input = load_input_as_list()
+
+
+def thirdify(input):
+    out = []
+    while input:
+        out.append(input[:3])
+        input = input[3:]
+    return out
+
+
+def doit(input):
+    xs = [set(x) & set(y) & set(z) for x, y, z in thirdify(input)]
+    return sum([ord(a) - 38 if a.isupper() else ord(a) - 96 for (a,) in xs])
+
+
+if __name__ == "__main__":
+    print(doit(input))
