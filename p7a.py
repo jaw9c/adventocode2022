2108ee8b83372f742602dcdf21e4f0f87aefac02
Josh Warwick <josh.warwick15@gmail.com>
2022-12-22 20:15:28 +0000

    day7

---
+++ p7a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from utils import *
+
+input = load_input_as_list()
+
+def construct(input):
+    state = {}
+    cwd_stack = [state]
+    for instr in input[1:]:
+        if instr[:1] == "$":
+            dir = instr[1:]
+            if dir[1:3] == "cd":
+                if dir[4:] == "..":
+                    cwd_stack.pop()
+                else:
+                    cwd_stack.append(cwd_stack[-1][dir[4:]])
+        else:
+            if instr[:3] == "dir":
+                cwd_stack[-1][instr[4:]] = {}
+            else:
+                size, name = instr.split(" ")
+                cwd_stack[-1][name] = int(size)
+    return state
+
+def search(node, log):
+    if type(node) == int:
+        return node
+    else:
+        total = sum([search(v, log) for k, v in node.items()])
+        log.append(total)
+        return total
+
+def doit(input):
+    log = []
+    search(construct(input), log)
+    return sum([v for v in log if v < 100000])
+
+if __name__ == "__main__":
+    print(doit(input))
